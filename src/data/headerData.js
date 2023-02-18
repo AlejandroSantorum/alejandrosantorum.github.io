7e21bc91caad0e72d9946626fcaede54b3b19b49
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-02-18 16:42:38 +0000

    init webpage from template

---
+++ src/data/headerData.js
@@ -0,0 +1,13 @@
+import resume from '../assets/pdf/resume.pdf'
+import personalImg from '../assets/png/asantorum_github_pic.png'
+import personalSignatureImg from '../assets/png/asantorum_signature.png'
+
+export const headerData = {
+    name: 'Alejandro Santorum',
+    title: "Data Engineer and Data Scientist",
+    desciption:"MPhil. in Machine Learning and Machine Intelligence at the University of Cambridge. Computer Science and Mathematics graduate at UAM.",
+    //image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: personalImg,
+    signatureImg: personalSignatureImg,
+    resumePdf: resume
+}
---
+++ src/data/socialsData.js
@@ -0,0 +1,11 @@
+export const socialsData = {
+    email: "mailto:alejandro.santorum@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/alejandro-santorum/",
+    github: "https://github.com/AlejandroSantorum",
+    // facebook: 'https://www.facebook.com/',
+    // instagram: 'https://www.instagram.com/',
+    // youtube: 'https://youtube.com/'
+    // medium: 'https://medium.com/@',
+    // stackOverflow: 'https://stackoverflow.com/users/',
+    // gitlab: 'https://gitlab.com/',
+}
