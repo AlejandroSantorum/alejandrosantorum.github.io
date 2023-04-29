afe4125826cf3f5bbde6c953867f51f557e6ebab
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-04-29 21:00:28 +0100

    Restoring favicon

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -13,7 +13,7 @@ function Main() {
     return (
         <div>
             <Helmet>
-                <title>{headerData.name} {headerData.second_surname}</title>
+                <title>{headerData.name} - Porfolio</title>
             </Helmet>
 
             <Navbar />        
