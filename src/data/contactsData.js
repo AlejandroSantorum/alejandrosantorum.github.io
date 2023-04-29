8936ca431a7757b336b356b245ed6aa71ae2d52e
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-04-29 11:27:09 +0100

    Improved Contacts section: flex-col width and font-size

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'alejandro.santorum@gmail.com',
     phone: '+34 672 779 997',
-    address: 'Camden Town, NW1 0AT London, UK.',
+    address: 'Camden Town, NW1 0AT London, United Kingdom.',
 
     sheetAPI: ''
 }
