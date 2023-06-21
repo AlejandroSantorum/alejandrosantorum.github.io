ab3d4aa24fadb2d339449e5653357fe728b47a7d
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-06-21 17:51:11 +0100

    Improved Contacts section to send me an email whenever someone contacts me

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -3,5 +3,5 @@ export const contactsData = {
     phone: '+34 672 779 997',
     address: 'Camden Town, NW1 0AT London, United Kingdom.',
 
-    sheetAPI: `https://formsubmit.co/ajax/${email}`
+    sheetAPI: 'https://formsubmit.co/ajax/f7f310a003949598dede033a26889519'
 }
