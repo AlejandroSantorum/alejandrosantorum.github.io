a0a76d8066a979f09ececb3c05023343ae23b9f6
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-06-22 13:38:24 +0100

    Add drone certification

--- src/data/certificationData.js
+++ src/data/certificationData.js
@@ -13,6 +13,16 @@ export const certificationData = {
         },
         {
             id : 2,
+            title : 'RPAS Pilot - EASA Drone certification',
+            title_url : 'https://www.easa.europa.eu/en/the-agency/faqs/drones-uas',
+            details : 'TODO.',
+            date : 'Dec. 2021',
+            credential_id : 'AESASGEEPGEE000F0HPK9A08JTAADA',
+            credential_url : 'https://sede.seguridadaerea.gob.es/CID/',
+            image : 'https://www.easa.europa.eu/themes/custom/easa_foundation/logo.svg'
+        },
+        {
+            id : 3,
             title : 'Deep Learning Specialization',
             title_url : 'https://www.coursera.org/specializations/deep-learning',
             details : 'TODO.',
@@ -22,7 +32,7 @@ export const certificationData = {
             image : 'https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/b4/5cb90bb92f420b99bf323a0356f451/Icon.png?auto=format%2Ccompress&dpr=2&w=80&h=80'
         },
         {
-            id : 3,
+            id : 4,
             title : 'IBM: Deep Learning with Python and PyTorch',
             title_url : 'https://www.edx.org/course/deep-learning-with-python-and-pytorch',
             details : 'TODO.',
@@ -32,7 +42,7 @@ export const certificationData = {
             image : 'https://www.edx.org/images/logos/edx-logo-elm.svg'
         },
         {
-            id : 4,
+            id : 5,
             title : 'HarvardX: High-Dimensional Data Analysis',
             title_url : 'https://www.edx.org/course/high-dimensional-data-analysis',
             details : 'TODO.',
@@ -40,8 +50,17 @@ export const certificationData = {
             credential_id : '32d1186cf81e4e4fadfe804f6e84e330',
             credential_url : 'https://courses.edx.org/certificates/32d1186cf81e4e4fadfe804f6e84e330',
             image : 'https://www.edx.org/images/logos/edx-logo-elm.svg'
+        },
+        {
+            id : 6,
+            title : 'Stanford Online: Machine Learning',
+            title_url : 'https://www.coursera.org/specializations/machine-learning-introduction',
+            details : 'TODO.',
+            date : 'Apr. 2018',
+            credential_id : 'X8S9XG45X34L',
+            credential_url : 'https://coursera.org/share/f8134afd0ad06b2b03346f5bbc202e9a',
+            image : 'https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/http://coursera-university-assets.s3.amazonaws.com/e8/7cc3d09d3f11e698dfff46d35f2da1/Stanford_Coursera_Logo.png?auto=format%2Ccompress&dpr=2&w=80&h=80'
         }
-
     ]
 }
 
