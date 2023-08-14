8d9a73b853b9047a6b82c52132efa10c9a1f39ba
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-08-14 23:33:16 +0100

    Update Contact data to remove precise address

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'alejandro.santorum@gmail.com',
     phone: '+34 672 779 997',
-    address: 'Camden Town, NW1 0AT London, United Kingdom.',
+    address: 'London, United Kingdom.',
 
     sheetAPI: 'https://formsubmit.co/ajax/f7f310a003949598dede033a26889519'
 }
